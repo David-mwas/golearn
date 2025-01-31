774812644eefbe7932d46626fbe12c0a7c6dd63e
David-mwas <dmwas704@gmaIl.com>
2025-01-31 19:16:30 +0300

    Get all courses,add course,update a course

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/David-mwas/golearn
 
 go 1.23.5
 
+require golang.org/x/exp v0.0.0-20250128182459-e0ece0dbea4c
+
 require (
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
@@ -13,6 +15,6 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	go.mongodb.org/mongo-driver v1.17.2 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,20 +2,113 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/exp/rand"
 )
+
+type Course struct {
+	CourseID    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice float64 `json:"courseprice"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	FullName string `json:"fullname"`
+	WebSite  string `json:"website"`
+}
+
+// fake db
+var courses []Course
+
+// middleware,helper functions
+func (c *Course) isEmpty() bool {
+	// return c.CourseID == "" && c.CourseName == ""
+
+	return c.CourseName == ""
+}
 
 func main() {
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", serveHome).Methods("GET")
-
-	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
+// controllers
+
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	w.Write([]byte("<h1>Welcome to the home page</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get a course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// get the course id from the url
+	courseid := mux.Vars(r)["courseid"]
+
+	for _, course := range courses {
+		if course.CourseID == courseid {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+
+	}
+	json.NewEncoder(w).Encode("Course with id: " + courseid + " not found")
+	// return
+}
+
+func createCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create a course")
+	w.Header().Set("Content-Type", "application/json")
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please provide course details")
+		return
+	}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.isEmpty() {
+		json.NewEncoder(w).Encode("Please provide course details")
+		return
+	}
+
+	// generate a course ,string
+	rand.Seed(uint64(time.Now().UnixNano()))
+	course.CourseID = strconv.Itoa(rand.Intn(100))
+
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+
+}
+
+func updateCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update a course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// get the course id from the url
+	courseid := mux.Vars(r)["courseid"]
+
+	for index, course := range courses {
+		if course.CourseID == courseid {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseID = courseid
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+		}
+
+	}
+	json.NewEncoder(w).Encode("Course with id: " + courseid + " not found")
 }
